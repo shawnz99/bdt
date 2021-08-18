56cf2fd809d2fa8dc5f43671739a98f129f98a8a
Shawn Zimmermann <zimmerm3@buffalo.edu>
2021-08-18 18:21:10 -0400

    started on colors

--- bdt.c
+++ bdt.c
@@ -16,7 +16,13 @@
 
 
 #define BYTEPL 16
+
+
+#define ANSI_RESET  "/x1b[0m"
+#define ANSI_FG_RED "/x1b[31m"
+
 bool arrcomp(char a[], char b[]);
+void printBytes(char a[], char b[], int offset);
 
 int main (int argc, char **argv)
 {
@@ -28,33 +34,27 @@ int main (int argc, char **argv)
         exit(1);
     }
 
-    // file pointers
+    /*
+     * file pointers
+     */
     FILE *fptr1     = NULL;
     FILE *fptr2     = NULL;
-    // file bytes
-//    int *f1         = NULL;
-//    int *f2         = NULL;
 
-    char bytearr1[BYTEPL];
-    char bytearr2[BYTEPL];
+    /*
+     *  + 1 because if not it overwrites the 
+     *  first byte when it hits 16
+     */ 
+    char bytearr1[BYTEPL + 1];
+    char bytearr2[BYTEPL + 1];
 
     int byteOffset = 0;
+    int diffByte   = 0;
 
     /*
      *  If the number of args is correct then get some file ptrs
      */
-
     fptr1 = fopen(*(argv + 1), "r");
     fptr2 = fopen(*(argv + 2), "r");
-
-
-    /*
-     *  Getting some stuff to read the bytes into
-     */
-
-    //f1 = (int*) malloc(sizeof(char));
-    //f2 = (int*) malloc(sizeof(char));
-    
 
     /*
      *  Snagging the first byte first because
@@ -74,15 +74,13 @@ int main (int argc, char **argv)
 
         byteOffset++;
 
-
-        if ( byteOffset == BYTEPL && arrcomp(bytearr1, bytearr2) == true ) {
-            fprintf(stdout, "hello? \n");
-            
-        }
-        else if ( arrcomp(bytearr1, bytearr2) == false ) {
-            fprintf(stdout, "not the same \n");
-            exit(1);
-        }
+    }
+    if ( byteOffset == BYTEPL && arrcomp(bytearr1, bytearr2) == true ) {
+        fprintf(stdout, "hello? \n");
+    }
+    else if ( arrcomp(bytearr1, bytearr2) == false ) {
+        printBytes(bytearr1, bytearr2, byteOffset);
+        exit(1);
     }
 
     return 0;
@@ -93,6 +91,8 @@ int main (int argc, char **argv)
  *  function to go through each element of the arrays
  *  and compare them if theyre differnt return false
  */
+
+// TODO: have it put the actual byte where the difference is 
 bool arrcomp(char a[], char b[]) {
     
     for (int i = 0; i < BYTEPL; i++ ) {
@@ -101,4 +101,28 @@ bool arrcomp(char a[], char b[]) {
     }
 
     return true;
+}
+
+
+/*
+ *  Function to print the 16 byte chunk where the error resides
+ */
+void printBytes(char a[], char b[], int offset) {
+    
+    fprintf(stdout, "%08x:  ", offset);
+    for ( int i = 0; i < BYTEPL; i++ ) {
+        fprintf(stdout, "%02x", a[i]);
+        if ( i % 2 == 1 ) {
+            fprintf(stdout, " ");
+        }
+    }
+    fprintf(stdout, "| ");
+    for ( int i = 0; i < BYTEPL; i++ ) {
+        fprintf(stdout, "%02x", b[i]);
+        if ( i % 2 == 1 ) {
+            fprintf(stdout, " ");
+        }
+    }
+    fprintf(stdout, "\n");
+    
 }
